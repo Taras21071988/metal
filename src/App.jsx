e3607797dafa6cb5249f04c6c80a656e6230044c
Taras21071988 <tsasojlov@icloud.com>
2024-02-02 15:17:35 +0200

    start transferring data to a primitive database

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import "./App.scss";
 import Footer from "./components/footer/Footer";
 import Header from "./components/header/header";
 import Home from "./pages/Home/Home";
-
 function App() {
   return (
     <>
